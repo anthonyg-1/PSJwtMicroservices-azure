42742dc661b40cc62a54697c2e0244f3e956bec8
Tony Guimelli <tony.guimelli@gmail.com>
2023-04-06 13:43:55 -0400

    Update validatejwt.ps1

--- validatejwt.ps1
+++ validatejwt.ps1
@@ -22,9 +22,23 @@ try {
     if ($authHeader -match "Bearer") {
         $jwt = ($authHeader -split " ")[1];
 
-        [bool]$isAuthenticated = Test-JsonWebToken -JsonWebToken $jwt -HashAlgorithm SHA256 -Uri $jwkUri -ErrorAction Stop;
-        if ($isAuthenticated) {
-            $body = @{status = "Authenticated" } | ConvertTo-Json;
+        # Determine the signing algorithm:
+        $algConversionTable = @{RS256 = "SHA256"; RS384 = "SHA384"; RS512 = "SHA512" }
+        $discoveredAlg = $jwt | Get-JsonWebTokenHeader | Select-Object -ExpandProperty alg
+
+        [string]$algToUse = ""
+        if ($discoveredAlg -in $algConversionTable.Keys) {
+            $algToUse = $algConversionTable.Item($discoveredAlg)
+
+            [bool]$isAuthenticated = Test-JsonWebToken -JsonWebToken $jwt -HashAlgorithm $algToUse -Uri $jwkUri -ErrorAction Stop;
+            if ($isAuthenticated) {
+                $claims = Get-JsonWebTokenPayload -JsonWebToken $jwt
+                $body = [ordered]@{status = "Authenticated"; claims = $claims } | ConvertTo-Json;
+            }
+        }
+        else {
+            $body = $body = [ordered]@{status = "Access denied"; reason = "Invalid algorithm specified" } | ConvertTo-Json;
+            $statusCode = [HttpStatusCode]::Forbidden;
         }
     }
     else {
